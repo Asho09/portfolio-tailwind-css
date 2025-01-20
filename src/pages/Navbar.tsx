0b8e68732e7ce6595b877c24c1ec48783f946338
Asho09 <aash69115@gmail.com>
2025-01-21 03:40:09 +0500

    Update Navbar.tsx

--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -9,10 +9,10 @@ const Navbar = () => {
       <div className='flex justify-between items-center'>
         <div className=' text-5xl text-bold font-medium'>AYESHA</div>
         <ul className=' flex justify-end mr-5px text-3xl gap-10 lg:gap-10 '>
-          <li className='menuLink'><a href="/">Home</a></li>
-          <li className='menuLink'><a href="../../pages/About">About</a></li>
-          <li className='menuLink'><a href="/Skills">Skills</a></li>
-          <li className='menuLink'><a href="/Contact">Contact</a></li>
+          <li className='menuLink'><Link href="/">Home</Link></li>
+          <li className='menuLink'><Link href="../../pages/About">About</Link></li>
+          <li className='menuLink'><Link href="/Skills">Skills</Link></li>
+          <li className='menuLink'><Link href="/Contact">Contact</Link></li>
    
         </ul>
         <GiHamburgerMenu className='md:hidden'size={30}/>
